3b6471b3d5b2f22f86042d92383dc37b70ca52da
Pablo Cardozo <cardozopabloj@gmail.com>
2022-09-24 20:36:49 -0300

    database

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 
-$hostname = "localhost";
-$database = "siames";
+$hostname = "06.189.187.214";
+$database = "siames-store";
 $username = "root";
-$password = "";
+$password = "s1amesStore";
 
 $mysqli = mysqli_connect($hostname, $username, $password, $database); 
 
