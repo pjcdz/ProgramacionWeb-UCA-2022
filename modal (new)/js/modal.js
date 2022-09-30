2b7a252c284fb09db62a1b027ddb6c0757f72734
santipalla3 <111574863+santipalla3@users.noreply.github.com>
2022-09-30 11:01:57 -0300

    Add files via upload

--- modal (new)/js/modal.js
+++ modal (new)/js/modal.js
@@ -1,14 +1,14 @@
 // Get the modal
 
 var createmodal = document.getElementsByClassName("container-all")[0];
+// Get the <span> element that closes the modal
+
+var cancelar = document.getElementById("cerrar");
 
 
 // Get the button that opens the modal
 // var btn = document.getElementsByClassName("btn-edit")[0];
 var btncreate = document.getElementsByClassName("link")[0];
-
-// Get the <span> element that closes the modal
-
 
 // When the user clicks the button, open the modal 
 btncreate.onclick = function() {
@@ -20,4 +20,8 @@ window.onclick = function(event) {
     if (event.target == createmodal) {
         createmodal.style.display = "none";
     }
+}
+// When the user clicks on <span> (x), close the modal
+cancelar.onclick = function() {
+    createmodal.style.display = "none";
 }
